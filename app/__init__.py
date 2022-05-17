9d29e5ec8c09824787fc2f44021c93439ce0f0df
“Edu58” <edumuriithi58@gmail.com>
2022-05-17 13:48:19 +0300

    basic server setup

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from config import config_option
+from flask_bootstrap import Bootstrap5
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import IMAGES, UploadSet, configure_uploads
+
+db = SQLAlchemy()
+login = LoginManager()
+photos = UploadSet('photos', IMAGES)
+bootstrap = Bootstrap5()
+moment = Moment()
+
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config_option[config_name])
+
+    db.init_app(app)
+
+    bootstrap.init_app(app)
+    moment.init_app(app)
+
+    configure_uploads(app, photos)
+
+    from app.main import main
+    app.register_blueprint(main)
+
+    from app.auth import auth
+    app.register_blueprint(auth, url_prefix="/auth/")
+
+    return app
---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+
+class Config:
+    #SECRET_KEY = os.environ.get("SECRET_KEY")
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+
+class DevConfig(Config):
+    DEBUG = True
+    
+
+class ProdConfig(Config):
+    DEBUG = False
+    # uri = os.environ.get("DATABASE_URL")  # or other relevant config var
+    # if uri.startswith("postgres://"):
+    #     uri = uri.replace("postgres://", "postgresql://", 1)
+    # # rest of connection code using the connection string `uri`
+    # SQLALCHEMY_DATABASE_URI = uri
+
+
+config_option = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'default': DevConfig
+}
