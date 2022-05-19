e8bb3be2b560ec31af6cfd045cc754c687a051d6
“Edu58” <edumuriithi58@gmail.com>
2022-05-19 16:10:53 +0300

    adds vacation_details route and page

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField, StringField, IntegerField, DateTimeLocalField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms.validators import DataRequired, length
+from wtforms.validators import DataRequired, length, Email
 
 
 class NewDestination(FlaskForm):
@@ -12,17 +12,12 @@ class NewDestination(FlaskForm):
     duration = IntegerField('Duration of your stay', validators=[DataRequired()])
     amount_spent = IntegerField('Total amount spent', validators=[DataRequired()])
     submit = SubmitField('submit')
-from wtforms import StringField, TextAreaField, SubmitField, validators
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-# from wtforms.validators import DataRequired, Email, length, ValidationError
-
 
 
 class ContactForm(FlaskForm):
-    firstName =StringField('First Name', [validators.DataRequired("Enter your first name")])
-    lastName =StringField('Last Name', [validators.DataRequired("Enter your last name")])
-    email = StringField('E-mail', [validators.DataRequired("Enter a valid email address"), validators.Email("Enter a valid email address")])
-    subject = StringField('Subject', [validators.DataRequired("What's the nature of your message?")])
-    message = TextAreaField('Message', [validators.DataRequired("Didn't you want to say something?")])
+    firstName = StringField('First Name', validators=[DataRequired("Enter your first name")])
+    lastName = StringField('Last Name', validators=[DataRequired("Enter your last name")])
+    email = StringField('E-mail', validators=[DataRequired("Enter a valid email address"), Email()])
+    subject = StringField('Subject', validators=[DataRequired("What's the nature of your message?")])
+    message = TextAreaField('Message', validators=[DataRequired("Didn't you want to say something?")])
     submit = SubmitField('Send')
