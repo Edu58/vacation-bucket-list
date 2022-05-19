13ba31509e6bcee1ed9cf8eda2be5618b5f4f16e
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-05-19 11:11:25 +0300

    add routing

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,8 @@ from flask import render_template, request, redirect, url_for, flash
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/about')
+def about():
+    return render_template('about.html')
