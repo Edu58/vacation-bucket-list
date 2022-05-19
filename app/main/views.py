6a57a3d2aa71d6fb60cad5a21666a44f17c0c32e
“Edu58” <edumuriithi58@gmail.com>
2022-05-19 13:35:10 +0300

    feat: adds add_vacation route

--- app/main/views.py
+++ app/main/views.py
@@ -4,15 +4,24 @@ from flask import render_template, request, redirect, url_for, flash
 
 
 @main.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
 
 @main.route('/about')
+@login_required
 def about():
     return render_template('about.html')
 
 
+@main.route('/add-vacation', methods=["GET", "POST"])
+@login_required
+def add_vacation():
+    return render_template('vacations-list.html')
+
+
 @main.route('/vacations-list', methods=["GET", "POST"])
+@login_required
 def vacations_list():
     return render_template('vacations-list.html')
