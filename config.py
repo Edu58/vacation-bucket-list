a4043b83d20691d8f6eb6df2c912ec51f5f26f79
“Edu58” <edumuriithi58@gmail.com>
2022-05-20 09:24:41 +0300

    fix: remove unnecessary git files

--- config.py
+++ config.py
@@ -2,15 +2,11 @@ import os
 
 
 class Config:
-    # SECRET_KEY = os.environ.get("SECRET_KEY")
-    SECRET_KEY = 'youshouldusethis'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    # SECRET_KEY = 'youshouldusethis'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    SQLALCHEMY_DATABASE_URI = "sqlite:///vacationdb"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///vacationdb"
     # SECRET_KEY = os.environ.get("SECRET_KEY")
-    SECRET_KEY = 'youshouldusethis'
-    #SECRET_KEY = os.environ.get("SECRET_KEY")
-    SECRET_KEY ='youshouldusethis'
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
     MAIL_SERVER = ''
     MAIL_PORT = 587
     MAIL_USE_SSL = False
@@ -25,11 +21,11 @@ class DevConfig(Config):
 
 class ProdConfig(Config):
     DEBUG = False
-    # uri = os.environ.get("DATABASE_URL")  # or other relevant config var
-    # if uri.startswith("postgres://"):
-    #     uri = uri.replace("postgres://", "postgresql://", 1)
-    # # rest of connection code using the connection string `uri`
-    # SQLALCHEMY_DATABASE_URI = uri
+    uri = os.environ.get("DATABASE_URL")  # or other relevant config var
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    # rest of connection code using the connection string `uri`
+    SQLALCHEMY_DATABASE_URI = uri
 
 
 config_option = {
